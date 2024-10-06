3ba1321257ccb9436c291bae9ff87d0bd790ec15
JARED ALEXANDER CASTILLO LACAYO <jared.castillo884@std.uni.edu.ni>
2024-10-06 15:49:22 -0600

    Se le pide que ingrese el nombre del estudiante

--- Trabajo1/Program.cs
+++ Trabajo1/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using Trabajo1;
+
+namespace tarea1
+{
+    class Program : Universidad
+    {
+        public static void Main(string[] args)
+        {
+            Universidad universidad = new Universidad();
+
+            Console.WriteLine("Ingrese el nombre del estudiante:");
+            Universidad.Estudiante.Nombre = Console.ReadLine();
+
+
+            Universidad.Estudiante.Asistencia.PrintMetodo();
+        }
+    }
+
+}
